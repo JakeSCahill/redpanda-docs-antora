735b02f3a09d30391e406d317cd6fbaa88d74f01
JakeSCahill <jake@redpanda.com>
2023-03-20 11:36:26 +0000

    Clean up comments and attribute names

--- start-scripts/build.sh
+++ start-scripts/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Set the CONSOLE_VERSION environment variable to the output of the getLatestConsoleVersion.js script
+# Get the latest Redpanda Console version
 export CONSOLE_VERSION=$(node start-scripts/getLatestConsoleVersion.js);
 echo $CONSOLE_VERSION;
 
-# Set the ALGOLIA_API_KEY, ALGOLIA_APP_ID, and ALGOLIA_INDEX_NAME environment variables
+# Set the ALGOLIA environment variables
 export ALGOLIA_API_KEY=2639b1b6c05670f52200b301c84866d9;
 export ALGOLIA_APP_ID=6YUV7845VT;
 export ALGOLIA_INDEX_NAME=redpanda;
 
-# Set the attribute variable only if CONSOLE_VERSION and REDPANDA_ARR are not empty
+# Set the latest-console-version attribute
 if [[ -n "$CONSOLE_VERSION" ]]; then
-  attribute="--attribute latest-console-verson=$CONSOLE_VERSION"
+  attribute="--attribute latest-console-version=$CONSOLE_VERSION"
 else
   attribute=""
 fi
 
-# Run the antora command with the attribute variable and the antora-playbook.yml file
+# Build the site
 antora --to-dir docs $attribute --fetch antora-playbook.yml
